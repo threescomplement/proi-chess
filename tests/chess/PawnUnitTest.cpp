c279aa00937baaee1ef5a0af2c1a86b4d04f0cc5
Mikołaj Garbowski <mikolaj.garbowski@gmail.com>
2023-05-17 18:27:12 +0200

    added test for two captures

--- tests/chess/PawnUnitTest.cpp
+++ tests/chess/PawnUnitTest.cpp
@@ -6,8 +6,20 @@
 
 namespace PawnUnitTest {
 
-    bool in(const std::vector<Move> &vec,const Move &val) {
+    bool in(const std::vector<Move> &vec, const Move &val) {
         return std::find(vec.begin(), vec.end(), val) != vec.end();
+    }
+
+    bool containsAll(std::vector<Move> const &container, std::vector<Move> const &contained) {
+        if (container.size() != contained.size()) {
+            return false;
+        }
+
+        return std::all_of(
+                contained.begin(),
+                contained.end(),
+                [container](Move const &move) { return in(container, move); }
+        );
     }
 
     TEST(Pawn, getMoves_basePositionTwoMoves) {
@@ -23,7 +35,17 @@ namespace PawnUnitTest {
 
 
     TEST(Pawn, getMoves_twoAttacksPossible) {
+        auto board = Board::fromFEN("rnbqkbnr/ppp1p1pp/8/3p1p2/4P3/8/PPPP1PPP/RNBQKBNR");
+        auto pawn = board->getField(Position(4, 5))->getPiece();
+        auto pawnMoves = pawn->getMoves();
 
+        auto expected = {
+                Move({4, 5}, {5, 4}, pawn, true),
+                Move({4, 5}, {5, 5}, pawn, false),
+                Move({4, 5}, {5, 6}, pawn, true)
+        };
+        ASSERT_EQ(3, pawnMoves.size());
+        ASSERT_TRUE(containsAll(pawnMoves, expected));
     }
 
     TEST(Pawn, getMoves_oneAttackPossible) {
@@ -34,8 +56,7 @@ namespace PawnUnitTest {
 
     }
 
-    TEST(Pawn, getMoves_fieldBehindAttackedProtected)
-    {
+    TEST(Pawn, getMoves_fieldBehindAttackedProtected) {
 
     }
 
