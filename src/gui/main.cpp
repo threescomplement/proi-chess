efb44560aea043955fd6cb0749a54c49d6f0ebd8
Michal Luszczek <michluszcz@gmail.com>
2023-05-19 00:01:39 +0200

    Added files generated by QT creator

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 #include "../chess/Game.h"
+#include "mainwindow.h"
 
-int main() {
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
     std::cout << "Hello" << std::endl;
     Game *game = new Game;
 
-    return 0;
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
