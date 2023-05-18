bfe42833e4833dcfaf52e76a187ae2f091e18c99
Maksym <01178511@pw.edu.pl>
2023-05-18 15:37:42 +0200

    Factored out getAllowedPositionsFromOffsets from oseł

--- src/chess/pieces/Piece.cpp
+++ src/chess/pieces/Piece.cpp
@@ -4,3 +4,20 @@
 Position Piece::getPosition() const {
     return this->getField()->getPosition();
 }
+
+std::vector<Position> Piece::getAllowedPositionsFromOffsets(const std::vector<std::pair<int, int>> &offsets) const {
+    std::vector<Position> targetPositions;
+
+    for (auto offset: offsets) {
+        try {
+            targetPositions.push_back(
+                    this->getField()
+                            ->getPosition()
+                            .positionWithOffset(offset.first, offset.second)
+            );
+        } catch (std::exception &e) {
+            continue;  // Skip if position with offset is out of bounds
+        }
+    }
+    return targetPositions;
+}
