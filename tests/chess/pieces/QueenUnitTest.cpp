25527a73bbd9230277f91cd1a73d617b1da41ba1
Maksym <01178511@pw.edu.pl>
2023-05-18 18:28:45 +0200

    created test file skeletons and modified CMakeLists.txt

---
+++ tests/chess/pieces/QueenUnitTest.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+#include "gtest/gtest.h"
+#include "../../../src/chess/Move.h"
+#include "../../../src/chess/Position.h"
+#include "../common.h"
+
+#include "../../../src/chess/pieces/Queen.h"
+using namespace ChessUnitTestCommon;
+
+namespace QueenUnitTest {
+
+}
---
+++ tests/chess/pieces/RookUnitTest.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+#include "gtest/gtest.h"
+#include "../../../src/chess/Move.h"
+#include "../../../src/chess/Position.h"
+#include "../common.h"
+
+#include "../../../src/chess/pieces/Rook.h"
+using namespace ChessUnitTestCommon;
+
+namespace RookUnitTest {
+
+}
