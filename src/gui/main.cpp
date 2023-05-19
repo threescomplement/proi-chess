b54c9a9d080dcdac75152c71a8f9c477d8d9a0eb
Mikołaj Garbowski <mikolaj.garbowski@gmail.com>
2023-05-19 11:29:46 +0200

    linking chess logic to gui

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -1,7 +1,9 @@
 #include <QApplication>
 #include <QPushButton>
+#include "../chess/Board.h"
 
 int main(int argc, char *argv[]) {
+    auto board = Board::emptyBoard();
     QApplication a(argc, argv);
     QPushButton button("Hello world!", nullptr);
     button.resize(200, 100);
