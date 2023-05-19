5a3f6a214f3bbdc0dc6405b94bcaccaafa7cebc8
Michal Luszczek <michluszcz@gmail.com>
2023-05-19 23:10:36 +0200

    added an empty board jpg, tried to display it but it seems to only work with a direct path

--- src/gui/mainwindow.cpp
+++ src/gui/mainwindow.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QPixmap>
 
 MainWindow::MainWindow(QWidget *parent)
         : QMainWindow(parent)
         , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QPixmap board_map (":/resources/Empty_Board.jpg");
+    ui->GameBoard->setPixmap(board_map);
 }
 
 MainWindow::~MainWindow()
