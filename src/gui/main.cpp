52f170bdfcd30812d159729a8420fe2ff12d7f80
Michal Luszczek <michluszcz@gmail.com>
2023-05-19 15:52:55 +0200

    added files to be in line with what QT Creator works with

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -1,14 +1,10 @@
+#include "mainwindow.h"
 #include <QApplication>
-#include <QPushButton>
-#include <Board.h>
-#include <iostream>
 
-int main(int argc, char *argv[]) {
-    auto board = Board::emptyBoard();
-    std::cout << board->toFEN();
+int main(int argc, char *argv[])
+{
     QApplication a(argc, argv);
-    QPushButton button("Hello world!", nullptr);
-    button.resize(200, 100);
-    button.show();
-    return QApplication::exec();
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
