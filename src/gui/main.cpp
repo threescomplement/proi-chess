b2ac67e7aaf1ecd4f60e2256d14f66ea7eb70329
Mikołaj Garbowski <mikolaj.garbowski@gmail.com>
2023-05-19 11:42:12 +0200

    changed project structure to use libraries

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -1,9 +1,11 @@
 #include <QApplication>
 #include <QPushButton>
 #include <Board.h>
+#include <iostream>
 
 int main(int argc, char *argv[]) {
     auto board = Board::emptyBoard();
+    std::cout << board->toFEN();
     QApplication a(argc, argv);
     QPushButton button("Hello world!", nullptr);
     button.resize(200, 100);
