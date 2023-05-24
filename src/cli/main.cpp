414b1365dcb0e4f3475867d87557d2c29bfd11b6
Mikołaj Garbowski <mikolaj.garbowski@gmail.com>
2023-05-24 12:55:11 +0200

    play against player or bot in CLI

--- src/cli/main.cpp
+++ src/cli/main.cpp
@@ -8,6 +8,7 @@
 #include "Position.h"
 #include "ChessExceptions.h"
 #include "CLIExceptions.h"
+#include "StockfishBot.h"
 
 
 std::string playerPrompt(const Game &game) {
@@ -37,9 +38,7 @@ Move parseMove(const std::string &moveStr, const Game &game) {
     return {sourcePosition, targetPosition, movedPiece, capturedPiece};
 }
 
-void playPlayerVersusPlayer() {
-    auto game = Game();
-
+void processPlayerTurn(Game &game) {
     while (true) {
         std::cout << game.getBoard()->toString() << std::endl;
         std::string moveStr;
@@ -51,16 +50,19 @@ void playPlayerVersusPlayer() {
             auto availableMoves = game.getMovesFrom(move.getFrom());
 
             if (std::find(availableMoves.begin(), availableMoves.end(), move) == availableMoves.end()) {
-                std::cout << "Illegal move " << moveStr << ", moves allowed from " << move.getFrom().toString() << ":"
-                          << std::endl;
-                for (auto avilableMove: availableMoves) {
-                    std::cout << avilableMove.toStockfishNotation() << " ";
+                std::cout << "Illegal move " << moveStr
+                          << ", moves allowed from " << move.getFrom().toString() << ":" << std::endl;
+
+                for (auto availableMove: availableMoves) {
+                    std::cout << availableMove.toStockfishNotation() << " ";
                 }
                 std::cout << std::endl;
                 continue;
             }
 
             game.makeMove(move);
+            break;
+
         } catch (const ChessException &e) {
             std::cout << moveStr << ": " << e.what() << std::endl;
             continue;
@@ -73,8 +75,66 @@ void playPlayerVersusPlayer() {
     }
 }
 
+void processBotTurn(Game &game, StockfishBot &bot) {
+    game.makeMove(bot.getBestNextMove());
+}
+
+void playPlayerVersusPlayer() {
+    auto game = Game();
+
+    while (true) {
+        processPlayerTurn(game);
+    }
+}
+
+void playPlayerVersusComputer() {
+    auto game = Game();
+    auto bot = StockfishBot(game);
+    Color botColor;
+
+    while (true) {
+        std::string choice;
+        std::cout << "Choose color (w/b): ";
+        std::cin >> choice;
+
+        if (choice[0] == 'w') {
+            botColor = Color::BLACK;
+            break;
+        } else if (choice[0] == 'b') {
+            botColor = Color::WHITE;
+            break;
+        } else {
+            std::cout << "Invalid choice, type either 'b' or 'w'" << std::endl;
+            continue;
+        }
+    }
+
+    while (true) {
+        if (game.getCurrentPlayer()->getColor() == botColor) {
+            processBotTurn(game, bot);
+        } else {
+            processPlayerTurn(game);
+        }
+    }
+}
+
 int main(int argc, char *argv[]) {
-    playPlayerVersusPlayer();
+    while (true) {
+        std::string choice;
+        std::cout << "1. Play against another player on the same keyboard" << std::endl;
+        std::cout << "2. Play against computer" << std::endl;
+        std::cout << "(1/2) > ";
+        std::cin >> choice;
+
+        if (choice[0] == '1') {
+            playPlayerVersusPlayer();
+        } else if (choice[0] == '2') {
+            playPlayerVersusComputer();
+        } else {
+            std::cout << "Invalid command, choose either '1' or '2'" << std::endl;
+            continue;
+        }
+    }
 
     // TODO: help command
     // TODO: give up command
