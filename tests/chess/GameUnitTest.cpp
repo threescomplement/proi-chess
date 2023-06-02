d58cd8d1066740f719f988eca1d8f50eabd59c4a
Michal Luszczek <michluszcz@gmail.com>
2023-06-02 18:47:02 +0200

    Added test for capturing a pawn after a double move

--- tests/chess/GameUnitTest.cpp
+++ tests/chess/GameUnitTest.cpp
@@ -60,6 +60,24 @@ namespace GameUnitTest {
         ASSERT_EQ(pos("b4"), onlyMove.getTo());
     }
 
+    TEST(Game, makeMoveCaptureWhileEnPassant) {
+        auto game = Game::fromFEN("rnbqkbnr/ppp1pppp/8/3p4/3P4/3Q4/PPP1PPPP/RNB1KBNR b KQkq - 0 2");
+        Move pawnMove = Move(pos("f7"), pos("f5"), game.getPiece(pos("f7")));
+        game.makeMove(pawnMove);
+
+
+        Move problematic_move = Move(pos("d3"), pos("f5"), game.getPiece(pos("d3")), game.getPiece(pos("f5")) );
+
+
+        std::vector<Move> legalMoves = game.getMovesFrom(pos("d3"));
+        ASSERT_TRUE(std::find(legalMoves.begin(), legalMoves.end(), problematic_move) != legalMoves.end());
+        game.makeMove(problematic_move);
+        ASSERT_EQ(game.toFEN(), "rnbqkbnr/ppp1p1pp/8/3p1Q2/3P4/8/PPP1PPPP/RNB1KBNR b KQkq - 0 3");
+
+        //ASSERT_EQ(pos("b4"), onlyMove.getTo());
+    }
+
+
     TEST(Game, fromFENStartingGame) {
         auto game = Game::fromFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
         auto newGame = Game();
