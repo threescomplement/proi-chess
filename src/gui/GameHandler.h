4cc539daf22709a3938f93a38f75b621325f4944
Michal Luszczek <michluszcz@gmail.com>
2023-06-06 16:49:23 +0200

    Moved some game related logic to a new GameHandler class

---
+++ src/gui/GameHandler.h
@@ -0,0 +1,82 @@
+
+#ifndef CHESS_GAMEHANDLER_H
+#define CHESS_GAMEHANDLER_H
+
+#include <vector>
+#include "ClickableLabel.h"
+#include "Game.h"
+#include "pieces/PieceType.h"
+#include "Move.h"
+#include "GameField.h"
+#include "Color.h"
+#include "../bot/StockfishBot.h"
+#include "Color.h"
+#include "Position.h"
+
+
+class GameHandler {
+
+private:
+    Game *game;
+    std::vector<Move> validMoves;
+    bool botGame;
+    ChessBot *stockfishBot;
+    Color botColor;
+
+
+public:
+    GameHandler();
+
+    explicit GameHandler(Game *game, bool isBotgame = false, Color botColor = Color::BLACK);
+
+    ~GameHandler() noexcept;
+
+    Game *getGame() const;
+
+    const std::vector<Move> &getValidMoves() const;
+
+    void clearMoves();
+
+    const std::vector<Move> &loadMovesFromPosition(Position position);
+
+    Color getCurrentPlayerColor();
+
+    void setBotDepth(int depth);
+
+    Piece *getPiece(Position position);
+
+    /**
+     * check whether a field is the endpoint of one of the moves that are currenly
+     * being considered
+     *
+     * @param position - the ending position of the desired move
+     * @return the move with an endpoint at the given field, or nullptr if such a move is not found
+     **/
+    Move *findMoveTo(const Position position);
+
+    void makeMove(Move const *move);
+
+    void newGame(bool botGame, Color bot_color = Color::BLACK,
+                 std::string const &fenNotation =
+                         "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+
+
+    void handleBotMove();
+
+    std::string getGameFen();
+
+    bool isStalemate();
+
+    bool isCheck(Color kingColor);
+
+    bool isMate();
+
+    bool belongsTo(Piece *piece, Player *player);
+
+    bool pieceBelongsToCurrent(Piece *piece);
+
+    bool fieldBelongsToCurrent(Position position);
+};
+
+
+#endif //CHESS_GAMEHANDLER_H
--- src/gui/mainwindow.cpp
+++ src/gui/mainwindow.cpp
@@ -1,21 +1,19 @@
 #include <QPixmap>
-#include <vector>
 #include <QDialog>
 #include <QInputDialog>
 #include <QDir>
 #include <QThread>
 #include <QMessageBox>
 #include <QClipboard>
-#include <QThread>
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "GameField.h"
 #include "constants.h"
 #include "Position.h"
 #include "Move.h"
-#include "pieces/Piece.h"
 #include "ChessExceptions.h"
 #include "Player.h"
+#include "GameHandler.h"
 
 #define BOARD_HEIGHT 8
 #define BOARD_WIDTH 8
@@ -23,8 +21,9 @@
 
 MainWindow::MainWindow(Game *game, QWidget *parent)
         : QMainWindow(parent), ui(new Ui::MainWindow),
-          game(game), pickedField(nullptr), botGame(false), stockfishBot(nullptr), botColor(Color::BLACK) {
+          pickedField(nullptr), botGame(false), stockfishBot(nullptr), botColor(Color::BLACK) {
 
+    gameHandler = new GameHandler(game);
     this->resize(400, 400);
     ui->setupUi(this);
     ui->GameLayout->setGeometry(QRect(0, 0, 400, 400));
@@ -37,7 +36,7 @@ MainWindow::MainWindow(Game *game, QWidget *parent)
 
 MainWindow::~MainWindow() {
     delete ui;
-    delete game;
+    delete gameHandler;
 }
 
 
@@ -83,63 +82,44 @@ void MainWindow::createBoard(Color side) {
 void MainWindow::updateBoardDisplay() {
     for (int row = 1; row <= BOARD_SIZE; row++) {
         for (int col = 1; col <= BOARD_SIZE; col++) {
-            auto piece = this->game->getPiece(Position(row, col));
+            auto piece = gameHandler->getPiece(Position(row, col));
             emit updateFieldPiece(col, row, piece);
         }
     }
-    std::string currentPlayer = ((game->getCurrentPlayer()->getColor() == Color::WHITE) ? "White" : "Black");
+    std::string currentPlayer = ((gameHandler->getCurrentPlayerColor() == Color::WHITE) ? "White" : "Black");
     currentPlayer += "'s turn";
     ui->statusbar->showMessage(QString::fromStdString(currentPlayer));
-    if (game->isMate()) {
-        ui->statusbar->showMessage("Checkmate!");
-    } else if (game->isCheck(game->getCurrentPlayer()->getColor())) {
-        ui->statusbar->showMessage("Check!");
-    } else if (game->isStalemate()) {
-        ui->statusbar->showMessage("Stalemate!");
+    validMoves = gameHandler->getValidMoves();
+    for (const auto &move: validMoves) {
+        int dest_x = move.getTo().getCol();
+        int dest_y = move.getTo().getRow();
+        emit updateFieldMark(dest_x, dest_y, true);
     }
 }
 
 
 void MainWindow::handleFieldClick(GameField *field) {
 
+    if (field == nullptr) {
+        changePickedField(nullptr);
+        return;
+    }
     // TODO - Could be cleaned up a bit
     if (field != pickedField) {
         bool validChoice = false;
         // check if there is a move to the chosen position
-        Move *correspondingMove = findMove(validMoves, field);
+        Position fieldPos(field->getY(), field->getX());
+        Move *correspondingMove = gameHandler->findMoveTo(fieldPos);
         if (correspondingMove != nullptr) {
             changePickedField(nullptr);
             makeMove(correspondingMove);
-            validChoice = true;
             // check if the field exists or if its just being reset
-        } else if (field != nullptr) {
-            // get piece from that field
-            Piece *piece = game->getPiece(Position(field->getY(), field->getX()));
-            if (piece != nullptr &&
-                game->getCurrentPlayer() != nullptr) { // check if the piece and current player exist
-                if (piece->getColor() == game->getCurrentPlayer()->getColor()) { // check if the piece belongs to player
-                    changePickedField(field);
-                    validChoice = true;
-                }
-            }
-
-        }
-        if (!validChoice) {
+        } else if (gameHandler->fieldBelongsToCurrent(fieldPos)) {
+            changePickedField(field);
+        } else {
             changePickedField(nullptr);
         }
     }
-
-}
-
-
-Move *MainWindow::findMove(const std::vector<Move> &moves, const GameField *field) {
-    for (auto move: moves) {
-        Position goal = move.getTo();
-        if (goal.getCol() == field->getX() && goal.getRow() == field->getY()) {
-            return new Move(move);
-        }
-    }
-    return nullptr;
 
 }
 
@@ -156,19 +136,19 @@ void MainWindow::makeMove(Move *move) {
                                                        {"Bishop", PieceType::BISHOP},
                                                        {"Queen",  PieceType::QUEEN}};
 
-        if (ok){
+        if (ok) {
             move->setPromotion(pickToPieceMap[pickedPiece]);
-            game->makeMove(*move);
+            gameHandler->makeMove(move);
         }
 
     } else {
-        game->makeMove(*move);
+        gameHandler->makeMove(move);
     }
 
     updateBoardDisplay();
     if (!(checkIfMate() || checkIfStalemate())) {
         //make bot move
-        handleBotMove();
+        gameHandler->handleBotMove();
         updateBoardDisplay();
         checkIfMate();
         checkIfStalemate();
@@ -184,11 +164,13 @@ void MainWindow::changePickedField(GameField *const new_picked) {
 
     }
     if (new_picked != nullptr) { // if the new picked is a field
-        validMoves = game->getLegalMovesFrom(Position(new_picked->getY(), new_picked->getX()));
+
+        gameHandler->loadMovesFromPosition(Position(new_picked->getY(), new_picked->getX()));
         // update the mark of the newly picked field
         emit updateFieldMark(new_picked->getX(), new_picked->getY(), true);
     } else {
-        validMoves.clear();
+        gameHandler->clearMoves();
+        //validMoves.clear();
     }
     emit callReset();
 
@@ -196,55 +178,38 @@ void MainWindow::changePickedField(GameField *const new_picked) {
     if (pickedField != nullptr) {
         emit updateFieldMark(pickedField->getX(), pickedField->getY(), true);
     }
-    for (const auto &move: validMoves) {
-        int dest_x = move.getTo().getCol();
-        int dest_y = move.getTo().getRow();
-        emit updateFieldMark(dest_x, dest_y, true);
-    }
+
     updateBoardDisplay();
 }
 
 void MainWindow::newGame(bool botGame, Color botColor, std::string fenNotation) {
-    Game *newGame = nullptr;
-    try {
-        newGame = new Game(Game::fromFEN(fenNotation));
-    } catch (FenException &e) {
-        delete newGame;
-        throw FenException("Incorrect Fen");
-    }
+    gameHandler->newGame(botGame, botColor, fenNotation);
 
     createBoard((botColor == Color::WHITE) ? Color::BLACK : Color::WHITE);
     changePickedField(nullptr);
 
-    delete stockfishBot;
-    delete game;
-
-    game = newGame;
-    this->botGame = botGame;
-    this->botColor = botColor;
-    if (this->botGame) {
-        stockfishBot = new StockfishBot(*game);
+    if (botGame) {
 
         int bot_depth = QInputDialog::getInt(this, tr("Bot difficulty"),
                                              tr("Enter the bot's search depth (the higher the depth,"
                                                 " the more accurate the bot will be):"),
                                              QLineEdit::Normal,
                                              0);
-        stockfishBot->setDepth(bot_depth);
+        gameHandler->setBotDepth(bot_depth);
     }
-    handleBotMove();
+    gameHandler->handleBotMove();
     updateBoardDisplay();
 }
 
 
-void MainWindow::newFenGame(bool botGame, Color bot_color) {
+void MainWindow::newFenGame(bool isBotGame, Color bot_color) {
     bool ok;
     QString fenNotation = QInputDialog::getText(this, tr("New Fen notation game"),
                                                 tr("Enter the FEN notation:"), QLineEdit::Normal,
                                                 QDir::home().dirName(), &ok);
     if (ok) {
         try {
-            newGame(botGame, bot_color, fenNotation.toStdString());
+            newGame(isBotGame, bot_color, fenNotation.toStdString());
         } catch (FenException) {
             // incorrect fen notation
             // display warning pop-up
@@ -292,17 +257,10 @@ void MainWindow::on_actionNew_bot_game_from_FEN_triggered() {
 }
 
 
-void MainWindow::handleBotMove() {
-    if (botGame && botColor == game->getCurrentPlayer()->getColor()) {
-
-        Move botMove = stockfishBot->getBestNextMove();
-        game->makeMove(botMove);
-    }
-}
-
 bool MainWindow::checkIfMate() {
-    if (game->isMate()) {
-        std::string winner = ((game->getCurrentPlayer()->getColor() == Color::WHITE) ? "Black" : "White");
+    bool isMate = gameHandler->isMate();
+    if (isMate) {
+        std::string winner = ((gameHandler->getCurrentPlayerColor() == Color::WHITE) ? "Black" : "White");
         winner += " is the winner!";
         QMessageBox::warning(
                 this,
@@ -311,13 +269,13 @@ bool MainWindow::checkIfMate() {
         ui->statusbar->showMessage("Checkmate!");
     }
 
-    return game->isMate();
+    return isMate;
 }
 
 
 void MainWindow::on_actionCopy_FEN_to_clipboard_triggered() {
     QClipboard *clipboard = QApplication::clipboard();
-    QString text = QString::fromStdString(game->toFEN());
+    QString text = QString::fromStdString(gameHandler->getGameFen());
     clipboard->setText(text, QClipboard::Clipboard);
 
     if (clipboard->supportsSelection()) {
@@ -330,7 +288,8 @@ void MainWindow::on_actionCopy_FEN_to_clipboard_triggered() {
 }
 
 bool MainWindow::checkIfStalemate() {
-    if (game->isStalemate()) {
+    bool isStalemate = gameHandler->isStalemate();
+    if (isStalemate) {
         QMessageBox::warning(
                 this,
                 tr("Game Over"),
@@ -338,6 +297,6 @@ bool MainWindow::checkIfStalemate() {
         ui->statusbar->showMessage("Stalemate!");
     }
 
-    return game->isStalemate();
+    return isStalemate;
 }
 
